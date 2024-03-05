1b800a67791449ea635b867c3161205d657c0644
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-03-05 04:49:00 +0000

    Sync LeetCode submission Runtime - 215 ms (8.25%), Memory - 13.6 MB (100.00%)

---
+++ 0164-maximum-gap/solution.c
@@ -0,0 +1,69 @@
+void merge(int *a,int l,int m,int h)
+{
+    int i=l,j=m+1,k=0;
+    int res[h-l+1];
+    while(i<=m && j<=h)
+    {
+        if(a[i]<=a[j])
+        {
+            res[k]=a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            res[k]=a[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=m)
+    {
+        res[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<=h)
+    {
+        res[k]=a[j];
+        j++;
+        k++;
+    }
+    k=0;
+    for(i=l;i<=h;i++)
+    {
+        a[i]=res[k];
+        k++;
+    }
+}
+void  merge_sort(int *a,int l,int h)
+{
+    if(l<h)
+    {
+        int m=(l+h)/2;
+        merge_sort(a,l,m);
+        merge_sort(a,m+1,h);
+        merge(a,l,m,h);
+    }
+}
+int maximumGap(int* nums, int numsSize) {
+    if(numsSize==1)
+    {
+        return 0;
+    }
+    merge_sort(nums,0,numsSize-1);
+    int mx=0;
+    for(int i=0;i<numsSize-1;i++)
+    {
+        if(nums[i+1]-nums[i]>mx)
+        {
+            mx=nums[i+1]-nums[i];
+            
+        }
+        /*if(nums[i+1]-nums[i]==0)
+        {
+            return 0;
+        }*/
+    }
+    return mx;
+}
