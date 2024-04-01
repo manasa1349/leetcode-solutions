6242b0393c91568c708184520b1ed8ee0a71576e
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-04-01 15:57:30 +0000

    Sync LeetCode submission Runtime - 2 ms (1.88%), Memory - 5.6 MB (100.00%)

--- 1610-xor-operation-in-an-array/solution.c
+++ 1610-xor-operation-in-an-array/solution.c
@@ -1,12 +1,10 @@
 int xorOperation(int n, int start) {
-    int a[1000]={0},k=start;
-    for(int i=0;i<n;i++)
-    {
-        a[i]=start+2*i;
+    int k = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int num = start + 2 * i;
+        k ^= num;
     }
-    for(int i=1;i<n;i++)
-    {
-        k=k^a[i];
-    }
-     return k;
+    
+    return k;
 }
