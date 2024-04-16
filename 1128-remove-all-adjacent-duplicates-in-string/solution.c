adfd8243cdb3367fe35f2b52d2af21384c182015
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-04-16 05:44:57 +0000

    Sync LeetCode submission Runtime - 5 ms (11.53%), Memory - 7.9 MB (100.00%)

---
+++ 1128-remove-all-adjacent-duplicates-in-string/solution.c
@@ -0,0 +1,19 @@
+char* removeDuplicates(char* s) {
+    char *st=malloc(strlen(s));
+    int top=-1;
+    //int size=strlen(s);
+    st[++top]=s[0];
+    for(int i=1;i<strlen(s);i++)
+    {
+        if(top!=-1&&s[i]==st[top] )
+        {
+            top--;
+        }
+        else
+        {
+            st[++top]=s[i];
+        }
+    }
+    st[++top]='\0';
+    return st;
+}
