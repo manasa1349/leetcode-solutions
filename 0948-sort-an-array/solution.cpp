bab6a4a6a1d666af59d0d08bde2af9d41bf482b2
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-09-17 05:55:14 +0000

    Sync LeetCode submission Runtime - 99 ms (59.41%), Memory - 70.5 MB (99.74%)

--- 0948-sort-an-array/solution.cpp
+++ 0948-sort-an-array/solution.cpp
@@ -1,64 +1,7 @@
 class Solution {
 public:
-void merge(int *a,int l,int m,int h)
-{
-    int i=l,j=m+1,k=0;
-    int res[h-l+1];
-    while(i<=m && j<=h)
-    {
-        if(a[i]<=a[j])
-        {
-            res[k]=a[i];
-            i++;
-            k++;
-        }
-        else
-        {
-            res[k]=a[j];
-            j++;
-            k++;
-        }
-    }
-    while(i<=m)
-    {
-        res[k]=a[i];
-        i++;
-        k++;
-    }
-    while(j<=h)
-    {
-        res[k]=a[j];
-        j++;
-        k++;
-    }
-    k=0;
-    for(i=l;i<=h;i++)
-    {
-        a[i]=res[k];
-        k++;
-    }
-}
-void  merge_sort(int *a,int l,int h)
-{
-    if(l<h)
-    {
-        int m=(l+h)/2;
-        merge_sort(a,l,m);
-        merge_sort(a,m+1,h);
-        merge(a,l,m,h);
-    }
-
-}
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        int a[n];
-        for(int i=0;i<n;i++){
-            a[i]=nums[i];
-        }
-        merge_sort(a,0,n-1);
-        for(int i=0;i<n;i++){
-            nums[i]=a[i];
-        }
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
