6c6f9bc12b50ebed2367c5c810bcdc778d40b9f4
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-10-05 15:26:31 +0000

    Sync LeetCode submission Runtime - 4 ms (1.82%), Memory - 19.6 MB (27.20%)

--- 0021-merge-two-sorted-lists/solution.cpp
+++ 0021-merge-two-sorted-lists/solution.cpp
@@ -16,34 +16,20 @@ public:
         ListNode *newhead=new ListNode();
         ListNode *prev=newhead;
         while(temp1 && temp2){
-            ListNode *neww=new ListNode();
             if(temp1->val<=temp2->val){
-                neww->val=temp1->val;
-                neww->next=NULL;
+                prev->next=temp1;
                 temp1=temp1->next;
             }else{
-                neww->val=temp2->val;
-                neww->next=NULL;
+                prev->next=temp2;
                 temp2=temp2->next;
             }
-            prev->next=neww;
-            prev=neww;
+            prev=prev->next;
         }
-        while(temp1){
-            ListNode *neww=new ListNode();
-            neww->val=temp1->val;
-            neww->next=NULL;
-            temp1=temp1->next;
-            prev->next=neww;
-            prev=neww;
+        if(temp1){
+            prev->next=temp1;
         }
-        while(temp2){
-            ListNode *neww=new ListNode();
-            neww->val=temp2->val;
-            neww->next=NULL;
-            temp2=temp2->next;
-            prev->next=neww;
-            prev=neww;
+        if(temp2){
+            prev->next=temp2;
         }
         return newhead->next;
     }
