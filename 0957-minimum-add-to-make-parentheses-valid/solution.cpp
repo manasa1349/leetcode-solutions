e4b5b5b9e6bb63c196dada6bd734405be42ada4e
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-10-09 16:04:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ 0957-minimum-add-to-make-parentheses-valid/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    cnt++;
+                }else{
+                    if(st.top()=='('){
+                        st.pop();
+                    }else{
+                        cnt++;
+                        st.pop();
+                    }
+                }
+            }
+        }
+        return cnt+st.size();
+    }
+};
