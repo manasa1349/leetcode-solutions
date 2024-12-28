13cc180d66eb2ad10ea1b0d8752da30d8ac98772
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2024-12-28 14:54:25 +0000

    Sync LeetCode submission Runtime - 3 ms (38.46%), Memory - 8.1 MB (45.21%)

--- 0009-palindrome-number/solution.c
+++ 0009-palindrome-number/solution.c
@@ -1,11 +1,11 @@
 bool isPalindrome(int x) {
-    if(x<0 || x!=0 && x%10 ==0 ) return false;
-    int rev=0;
-    while(x>rev)
+    //if(x<0 || x!=0 && x%10 ==0 ) return false;
+    long long rev=0;
+    int temp=x;
+    while(x>0)
     {
-    //int k=x%10;
-    rev=rev*10+x%10;
-    x/=10;
+        rev=rev*10+(x%10);
+        x/=10;
     }
-    return (rev==x || x==rev/10);
+    return (rev==temp );
 }
