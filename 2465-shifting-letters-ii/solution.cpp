ef9eca63dacdfe6ef52f74b3ee9f8bbd5f702c77
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-01-05 16:40:25 +0000

    Sync LeetCode submission Runtime - 4 ms (71.82%), Memory - 99.7 MB (33.82%)

--- 2465-shifting-letters-ii/solution.cpp
+++ 2465-shifting-letters-ii/solution.cpp
@@ -8,18 +8,13 @@ public:
         for(long long i=0;i<m;i++){
             long long l=shifts[i][0];
             long long r=shifts[i][1];
-            /*long long chng=shifts[i][2];
-            for(long long j=l;j<=r;j++){
-                (chng==0)?pre[j]+=(-1):pre[j]+=1;
-            }*/
             long long chng=(shifts[i][2]==1)?1:-1;
             pre[l]+=chng;
             pre[r+1]-=chng;
         }
         int curr=0;
-        for(int i=0;i<n;i++){
-            curr+=pre[i];
-            pre[i]=curr;
+        for(int i=1;i<n;i++){
+            pre[i]+=pre[i-1];
         }
         for(long long i=0;i<n;i++){
             int net=(pre[i]%26+26)%26;
