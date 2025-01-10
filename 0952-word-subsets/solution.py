9a3a7243c1b15bd2dfe91e009cdce74fe7c5101c
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-01-10 16:50:30 +0000

    Sync LeetCode submission Runtime - 414 ms (27.69%), Memory - 21.3 MB (51.14%)

---
+++ 0952-word-subsets/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        max_freq = Counter()
+        for word in words2:
+            word_count = Counter(word)
+            for char in word_count:
+                max_freq[char] = max(max_freq[char], word_count[char])    
+        result = []
+        for word in words1:
+            word_count = Counter(word)
+            if all(word_count[char] >= max_freq[char] for char in max_freq):
+                result.append(word)    
+        return result
