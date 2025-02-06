5b4f19967665f782b3fcf161f268cdd640ad493c
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-06 13:59:07 +0000

    Sync LeetCode submission Runtime - 800 ms (12.93%), Memory - 99.4 MB (15.59%)

---
+++ 1364-tuple-with-same-product/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int func(int n){
+        return (n*(n-1))/2;
+    }
+    int tupleSameProduct(vector<int>& nums) {
+        map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int pro=nums[i]*nums[j];
+                mp[pro]++;
+            }
+        }
+        int ans=0;
+        for(auto it:mp){
+            //cout<<it.first<<" "<<it.second<<" "<<ans<<endl;
+            if(it.second>1){
+                ans+=(func(it.second)*8);
+            }
+        }
+        return ans;
+    }
+};
