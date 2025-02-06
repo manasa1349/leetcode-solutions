aeb919267c791bc6ea562f8ec6dd2f9af0cf4ed5
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-06 14:19:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (86.29%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int func(int i,vector<int>&dp){
+    /*int func(int i,vector<int>&dp){
         if(i==0 || i==1) return 1;
         if(dp[i]!=-1) return dp[i];
         return dp[i]=func(i-1,dp)+func(i-2,dp);
-    }
+    }*/
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n,dp);
+        //vector<int>dp(n+1,-1);
+        if(n==1) return 1;
+        int prev1=1;
+        int prev2=1;
+        int ans=0;
+        for(int i=2;i<=n;i++){
+            ans=(prev1+prev2);
+            prev1=prev2;
+            prev2=ans;
+        }
+        return ans;
     }
 };
