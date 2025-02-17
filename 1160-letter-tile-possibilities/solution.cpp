9a7bfa5bd568322d51daf4d34603c2c8477a68a4
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-17 16:25:21 +0000

    Sync LeetCode submission Runtime - 42 ms (22.34%), Memory - 18.1 MB (20.26%)

---
+++ 1160-letter-tile-possibilities/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void generateSequences(string& tiles, string current, vector<bool>& used,
+unordered_set<string>& sequences) {
+        sequences.insert(current);
+        for (int pos = 0; pos < tiles.length(); ++pos) {
+            if (!used[pos]) {
+                used[pos] = true;
+                generateSequences(tiles, current + tiles[pos], used, sequences);
+                used[pos] = false;
+            }
+        }
+}
+    int numTilePossibilities(string tiles) {
+        unordered_set<string> sequences;
+        vector<bool> used(tiles.length(), false);
+        generateSequences(tiles, "", used, sequences);
+        return sequences.size() - 1;
+    }
+};
