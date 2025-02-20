06711547bfaca53e570c93fda88624c2f1c8cdcd
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-20 14:45:09 +0000

    Sync LeetCode submission Runtime - 57 ms (6.42%), Memory - 27.9 MB (5.50%)

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/solution.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/solution.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-void generateHappyStrings(int n, string &current, vector<string> &happyStrings) {
-        if (current.length() == n) {
-            happyStrings.push_back(current);
+    vector<string>res;
+    void func(string s,int n){
+        if(s.size()==n){
+            res.push_back(s);
             return;
         }
-        for (char ch : {'a', 'b', 'c'}) {
-            if (current.empty() || current.back() != ch) {
-                current.push_back(ch);
-                generateHappyStrings(n, current, happyStrings);
-                current.pop_back();
-            }
+        int l=s.size()-1;
+        if(s[l]=='a'){
+            func(s+'b',n);
+            func(s+'c',n);
+        }
+        else if(s[l]=='b'){
+            func(s+'c',n);
+            func(s+'a',n);
+        }else if(s[l]=='c'){
+            func(s+'a',n);
+            func(s+'b',n);
         }
     }
     string getHappyString(int n, int k) {
-        vector<string> happyStrings;
-        string current = "";        
-        generateHappyStrings(n, current, happyStrings);
-        return (k <= happyStrings.size()) ? happyStrings[k - 1] : "";
+        func("a",n);
+        func("b",n);
+        func("c",n);
+        sort(res.begin(),res.end());
+        if(res.size()>=k){
+            return res[k-1];
+        }
+        return "";
     }
 };
