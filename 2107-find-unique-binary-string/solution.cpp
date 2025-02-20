703f4ba508bb2a5e0e3c3d112f7c1575e59aad31
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-20 15:15:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (25.66%)

--- 2107-find-unique-binary-string/solution.cpp
+++ 2107-find-unique-binary-string/solution.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    /*vector<string>res;
-    void func(string s,int n){
+    string ans;
+    unordered_set<string>numset;
+    bool func(string s,int n){
         if(s.size()==n){
-            res.push_back(s);
-            return;
-        }
-        func(s+'0',n);
-        func(s+'1',n);
-    }*/
-    string findDifferentBinaryString(vector<string>& nums) {
-        /*func("0",nums.size());
-        func("1",nums.size());
-        unordered_map<string,int>mp;
-        for(int i=0;i<res.size();i++){
-            mp[res[i]]++;
-        }
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second==1){
-                return it.first;
+            if(numset.find(s)==numset.end()){
+                ans=s;
+                return true;
             }
+            return false;
         }
-        return "";*/
-        string ans = "";
+        if(func(s+'0',n)){
+            return true;
+        }
+        if(func(s+'1',n)){
+            return true;
+        }
+        return false;
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        numset=unordered_set(nums.begin(),nums.end());
+        if(func("",nums.size())){
+            return ans;
+        }
+        return "";
+        /*string ans = "";
         for (int i = 0; i < nums.size(); i++) {
             ans += (nums[i][i] == '0') ? '1' : '0';
         }
-        return ans;
+        return ans;*/
     }
 };
