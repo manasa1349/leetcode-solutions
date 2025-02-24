57a310624c8ebf94d32daa1ca547004349ffe2df
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-24 10:02:01 +0000

    Sync LeetCode submission Runtime - 643 ms (54.76%), Memory - 0.0B (100.00%)

---
+++ 0184-department-highest-salary/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with cte as(
+select d.name as department,e.name as employee,e.salary,
+RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
+from employee e
+join department d
+on e.departmentId=d.id)
+select department,employee,salary
+from cte
+where rnk=1;
