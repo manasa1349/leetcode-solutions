0f3c10112154a4ef1637cbc6f8fdec7a07614bd6
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-25 09:30:28 +0000

    Sync LeetCode submission Runtime - 557 ms (5.02%), Memory - 0.0B (100.00%)

--- 0626-exchange-seats/solution.sql
+++ 0626-exchange-seats/solution.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-with cte as(
-select id,student,
+select id,
 case 
 when mod(id,2)=1 then lead(student,1,student) over(order by id)
 else lag(student,1) over(order by id) 
-end as new_name
-from seat
-)
-select id,new_name as student from cte;
+end as student
+from seat;
