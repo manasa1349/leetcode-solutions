8cba0c6683329e621e8a647a76095dcdb19d4aea
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-02-28 04:25:52 +0000

    Sync LeetCode submission Runtime - 25 ms (60.03%), Memory - 26.7 MB (89.29%)

---
+++ 1170-shortest-common-supersequence-/solution.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.length(), n = str2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Step 1: Compute LCS length using DP
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1[i - 1] == str2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        // Step 2: Construct SCS from LCS
+        string result = "";
+        int i = m, j = n;
+
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                result += str1[i - 1];
+                i--;
+                j--;
+            } 
+            else if (dp[i - 1][j] > dp[i][j - 1]) {
+                result += str1[i - 1];
+                i--;
+            } 
+            else {
+                result += str2[j - 1];
+                j--;
+            }
+        }
+        while (i > 0) {
+            result += str1[i - 1];
+            i--;
+        }
+        while (j > 0) {
+            result += str2[j - 1];
+            j--;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
