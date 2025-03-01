fad7b270724b7cbad69279adcb418c7fabbc9ed6
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-03-01 08:07:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (25.82%)

---
+++ 2551-apply-operations-to-an-array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int j=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=j;i<n;i++){
+            nums[i]=0;
+        }
+        return nums;
+    }
+};
