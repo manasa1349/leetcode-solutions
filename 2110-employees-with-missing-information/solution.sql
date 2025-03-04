9dc9e220ff812c9aa8ddeb74d3086a8f508e2472
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-03-04 08:57:16 +0000

    Sync LeetCode submission Runtime - 391 ms (36.10%), Memory - 0.0B (100.00%)

---
+++ 2110-employees-with-missing-information/solution.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select COALESCE(e.employee_id,s.employee_id) as employee_id
+from employees e
+full outer join salaries s
+on e.employee_id=s.employee_id
+where e.name is null or s.salary is null
+order by employee_id;
