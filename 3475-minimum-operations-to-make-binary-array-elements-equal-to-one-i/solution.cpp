a0e12fa4ea6568e22b52907aff94e1686cd46a10
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-03-05 16:03:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 140.1 MB (44.68%)

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/solution.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/solution.cpp
@@ -3,7 +3,7 @@ public:
     int minOperations(vector<int>& nums) {
         int cnt=0;
         int n=nums.size();
-        for(int i=0;i<n-2;i++){
+        /*for(int i=0;i<n-2;i++){
             if(nums[i]==0){
                 nums[i]=!nums[i];
                 nums[i+1]=!nums[i+1];
@@ -12,6 +12,20 @@ public:
             }
         }
         if(nums[n-2]==0 ||nums[n-1]==0){
+            return -1;
+        }*/
+        int l=0,r=2;
+        while(r<n){
+            if(nums[l]==0){
+                nums[l]=!nums[l];
+                nums[l+1]=!nums[l+1];
+                nums[l+2]=!nums[l+2];
+                cnt++;
+            }
+            l++;
+            r++;
+        }
+        if(nums[r-1]==0 || nums[r-2]==0){
             return -1;
         }
         return cnt;
