95545c4bc2e9a240ba230eaefeb3b77b0a5e914d
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-05-11 13:02:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (58.28%)

--- 1293-three-consecutive-odds/solution.cpp
+++ 1293-three-consecutive-odds/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int p1=0,p2=1,p3=2;
-        while(p3<arr.size()){
-            if(arr[p1]%2!=0  && arr[p2]%2!=0 && arr[p3]%2!=0){
+        int cnt=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]%2!=0){
+                cnt++;
+            }else{
+                cnt=0;
+            }
+            if(cnt==3){
                 return true;
             }
-            p1++;
-            p2++;
-            p3++;
         }
         return false;
     }
