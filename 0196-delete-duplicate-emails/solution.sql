6812c66d77c9014f838c55c1d312bb6efe395102
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-09-16 18:13:35 +0000

    Sync LeetCode submission Runtime - 3361 ms (5.03%), Memory - 0.0B (100.00%)

--- 0196-delete-duplicate-emails/solution.sql
+++ 0196-delete-duplicate-emails/solution.sql
@@ -1,6 +1,19 @@
-/* Write your T-SQL query statement below */
-delete p1 from person p1
-join person p2
-on p1.id>p2.id and 
-p1.email=p2.email;
+-- /* Write your T-SQL query statement below */
+-- delete p1 from person p1
+-- join person p2
+-- on p1.id>p2.id and 
+-- p1.email=p2.email;
+
+
+WITH ranked AS (
+    SELECT 
+        id,
+        email,
+        ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+    FROM Person
+)
+DELETE FROM Person
+WHERE id IN (
+    SELECT id FROM ranked WHERE rn > 1
+);
 
