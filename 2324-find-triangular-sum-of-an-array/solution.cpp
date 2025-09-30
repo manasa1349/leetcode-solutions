57ded6721a9b985937d5c6b07cd159c333ddd358
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-09-30 08:55:32 +0000

    Sync LeetCode submission Runtime - 48 ms (88.38%), Memory - 22.1 MB (96.72%)

---
+++ 2324-find-triangular-sum-of-an-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        while(nums.size()>1){
+            int n=nums.size();
+            for(int i=0;i<n-1;i++){
+                nums[i]=(nums[i]+nums[i+1])%10;
+            }
+            nums.pop_back();
+        }
+        return nums[0];
+    }
+};
