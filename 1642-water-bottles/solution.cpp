d2fc0c81aaf722b973b763fcb55f0dd1495a91c2
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-10-01 08:58:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (79.39%)

--- 1642-water-bottles/solution.cpp
+++ 1642-water-bottles/solution.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int nb, int ne) {
-        int ans=nb;
-        int rem=nb;
-        while (rem >= ne) {
-            int newBottles = rem / ne;
-            ans += newBottles;
-            rem = (rem % ne) + newBottles;
-        }
-        return ans;
+    int numWaterBottles(int b, int e) {
+        return (b*e-1)/(e-1);
     }
 };
