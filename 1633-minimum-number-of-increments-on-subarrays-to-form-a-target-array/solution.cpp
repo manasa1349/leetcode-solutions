700fa1abfd4f6d5a4382564a361532471e3562ad
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-10-31 05:25:40 +0000

    Sync LeetCode submission Runtime - 22 ms (7.89%), Memory - 78.3 MB (12.66%)

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
-        int res=target[0];
-        for(int i=1;i<target.size();i++){
-            res+=(target[i]-target[i-1]>0)?(target[i]-target[i-1]):0;
+        int res = 0;
+        stack<int> st;
+        st.push(0);
+
+        for (int h : target) {
+            if (h > st.top()) {
+                res += (h - st.top());
+                st.push(h);
+            }else {
+                while (!st.empty() && st.top() > h) st.pop();
+                if (st.top() != h) st.push(h);
+            }
         }
         return res;
     }
