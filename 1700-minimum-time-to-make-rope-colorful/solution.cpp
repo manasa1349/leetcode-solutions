4e3530a41ea13a924558cda624bbb9f0ef31ab67
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-11-03 02:02:06 +0000

    Sync LeetCode submission Runtime - 3 ms (69.11%), Memory - 100 MB (32.82%)

--- 1700-minimum-time-to-make-rope-colorful/solution.cpp
+++ 1700-minimum-time-to-make-rope-colorful/solution.cpp
@@ -4,8 +4,8 @@ public:
         int ans=0;
         for(int i=1;i<c.size();i++){
             if(c[i-1]==c[i]){
-                ans+=min(t[i],t[i-1]);
-                t[i]=max(t[i],t[i-1]);
+                ans+=t[i]<t[i-1]?t[i]:t[i-1];
+                t[i]=t[i]>t[i-1]?t[i]:t[i-1];
             }
         }
         return ans;
