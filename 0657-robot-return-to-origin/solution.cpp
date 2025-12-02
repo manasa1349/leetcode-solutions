357a12c9ffba3db780d598f7e832761082607ee1
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-02 10:04:16 +0000

    Sync LeetCode submission Runtime - 4 ms (28.70%), Memory - 10.4 MB (45.13%)

--- 0657-robot-return-to-origin/solution.cpp
+++ 0657-robot-return-to-origin/solution.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         int u,d,l,r;
-        u=0,r=0,l=0,d=0;
+        u=0,r=0;
         for(int i=0;i<moves.size();i++){
             if(moves[i]=='U') u++;
-            else if(moves[i]=='L') l++;
+            else if(moves[i]=='L') r--;
             else if(moves[i]=='R') r++;
-            else if(moves[i]=='D') d++;
+            else if(moves[i]=='D') u--;
         }
-        return (l==r)&&(u==d);
+        return (r==0)&&(u==0);
     }
 };
