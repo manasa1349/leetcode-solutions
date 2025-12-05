2fb3e980743821bc769f036c5646396f47a89812
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-05 09:01:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.2 MB (7.65%)

--- 3704-count-partitions-with-even-sum-difference/solution.cpp
+++ 3704-count-partitions-with-even-sum-difference/solution.cpp
@@ -4,16 +4,12 @@ public:
         int n=nums.size();
         vector<int>pre(n);
         pre[0]=nums[0];
-        cout<<pre[0]<<" ";
         for(int i=1;i<n;i++){
             pre[i]=pre[i-1]+nums[i];
-            cout<<pre[i]<<" ";
         }
         int ans=0;
-        cout<<endl;
         for(int i=0;i<n-1;i++){
             if(abs(pre[i]-(pre[n-1]-pre[i]))%2==0){
-                cout<<pre[i]<<" ";
                 ans++;
             }
         }
