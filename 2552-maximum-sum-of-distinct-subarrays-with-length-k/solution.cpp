5f63163144f1834de2010c0dd74ac66cadae777b
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-23 13:04:00 +0000

    Sync LeetCode submission Runtime - 95 ms (85.14%), Memory - 95 MB (98.68%)

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/solution.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/solution.cpp
@@ -4,32 +4,25 @@ public:
         long long ans=0;
         long long sum=0;
         int n=nums.size();
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         int chk=0;
         for(int i=0;i<k;i++){
             mp[nums[i]]++;
             sum+=nums[i];
         }
-        chk=mp.size();
-        if(chk==k){
+        if(mp.size()==k){
             ans=sum;
         }
         int l=0,r=k;
         while(r<n){
-            if(mp[nums[l]] && mp[nums[l]]==1){
-                chk--;
-            }
             mp[nums[l]]--;
             if(mp[nums[l]]==0){
                 mp.erase(nums[l]);
             }
             mp[nums[r]]++;
-            if(mp[nums[r]]==1){
-                chk++;
-            }
             sum-=nums[l];
             sum+=nums[r];
-            if(chk==k){
+            if(mp.size()==k){
                 ans=max(ans,sum);
             }
             l++;
