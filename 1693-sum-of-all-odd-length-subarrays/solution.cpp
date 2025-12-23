bf0de5471d523da88187dac41452e50cb8c9e6b7
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-23 09:19:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.2 MB (24.54%)

---
+++ 1693-sum-of-all-odd-length-subarrays/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            sum+=ceil(((i+1)*(n-i))/2.0)*arr[i];
+        }
+        return sum;
+    }
+};
