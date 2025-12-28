2e6b14cd4126f151af651063c227b99a9cb5f11a
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-28 14:13:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14 MB (66.32%)

--- 1476-count-negative-numbers-in-a-sorted-matrix/solution.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/solution.cpp
@@ -5,8 +5,6 @@ public:
         for(int i=0;i<grid.size();i++){
             reverse(grid[i].begin(),grid[i].end());
             int idx=lower_bound(grid[i].begin(),grid[i].end(),0)-grid[i].begin();
-            cout<<idx<<" ";
-            // ans+=(grid[i].size()-idx);
             ans+=idx;
         }
         return ans;
