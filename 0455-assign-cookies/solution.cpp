abeaebc8eda08fe7a630508c18881dfa450e4440
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2025-12-30 14:28:13 +0000

    Sync LeetCode submission Runtime - 22 ms (5.87%), Memory - 44.9 MB (45.86%)

--- 0455-assign-cookies/solution.cpp
+++ 0455-assign-cookies/solution.cpp
@@ -3,17 +3,19 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(s.begin(),s.end());
         sort(g.begin(),g.end());
-        int j=0,cnt=0;
+        int j=0,i=0,cnt=0;
         int n=s.size();
-        for(int i=0;i<g.size();i++){
-            while(j<n && g[i]>s[j]){
-                j++;
-            }
-            if(j<n && g[i]<=s[j]){
+        int n1=g.size();
+        // for(int i=0;i<g.size();i++){
+        while(j<n && i<n1){
+            if(g[i]<=s[j]){
                 cnt++;
-                j++;
+                i++;
             }
+            j++;
         }
+            
+        // }
         return cnt;
     }
 };
