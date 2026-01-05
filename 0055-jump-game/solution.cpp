a1961706e5ba18259a5c0358a6ff640d27de223d
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-05 15:22:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.2 MB (90.12%)

--- 0055-jump-game/solution.cpp
+++ 0055-jump-game/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(n<=1) return true;
-        if(nums[0]==0) return false;
-        int cidx=0;
-        int gidx=0;
-        for(int i=0;i<n-1;i++){
-            gidx=max(i+nums[i],gidx);
-            if(gidx>=n-1){
-                return true;
-            }
-            if(i==cidx && i!=gidx){
-                cidx=gidx;
-            }else if(i==cidx && i==gidx){
-                return false;
-            }
-        }
-        return false;
+    int maxReach = 0;
+
+    for(int i = 0; i < nums.size(); i++) {
+        if(i > maxReach) return false;
+        maxReach = max(maxReach, i + nums[i]);
     }
+
+    return true;
+}
+
 };
+
