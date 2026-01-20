4cec1242c04c81f3f915fa2fce902c8676dfd1a7
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-20 08:48:51 +0000

    Sync LeetCode submission Runtime - 80 ms (5.32%), Memory - 116.6 MB (5.24%)

---
+++ 0525-contiguous-array/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                sum--;
+            }else{
+                sum++;
+            }
+            mp[sum].push_back(i);
+        }
+        int ans=0;
+        for(auto i:mp){
+            vector<int>arr=i.second;
+            if(i.first!=0){
+                int val=*max_element(arr.begin(),arr.end())-*min_element(arr.begin(),arr.end());
+                ans=max(ans,val);
+            }else{
+                int val=*max_element(arr.begin(),arr.end());
+                ans=max(ans,val+1);
+            }
+        }
+        return ans;
+    }
+};
