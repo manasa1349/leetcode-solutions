434e86010705235f511b13e7acd3e741a6ac7661
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-23 15:21:21 +0000

    Sync LeetCode submission Runtime - 139 ms (19.11%), Memory - 32.7 MB (66.54%)

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    bool func(int k,int m,int n,int threshold,vector<vector<int>>pre){
-        for(int i=0;i+k<=n;i++){
-            for(int j=0;j+k<=m;j++){
-                int val=pre[i+k][j+k]+pre[i][j]-pre[i+k][j]-pre[i][j+k];
-                if(val<=threshold){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>pre(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                pre[i][j]=pre[i-1][j]+pre[i][j-1]+mat[i-1][j-1]-pre[i-1][j-1];//mat->0-indexed, pre->1-indexed
+                pre[i][j]=pre[i-1][j]+pre[i][j-1]+mat[i-1][j-1]-pre[i-1][j-1];
             }
         }
-        int l=1,h=min(m,n);
+        // int k=1;
         int ans=0;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(func(mid,m,n,threshold,pre)){
-                ans=mid;
-                l=mid+1;
-            }else{
-                h=mid-1;
+        for(int k=1;k<=min(m,n);k++){
+            bool chk=false;
+            for(int i=0;i+k<=n;i++){
+                for(int j=0;j+k<=m;j++){
+                    int val=pre[i+k][j+k]+pre[i][j]-pre[i+k][j]-pre[i][j+k];
+                    if(val<=threshold){
+                        chk=true;
+                        ans=k;
+                        break;
+                    }
+                }
+                if(chk){
+                    break;
+                }
             }
         }
         return ans;
