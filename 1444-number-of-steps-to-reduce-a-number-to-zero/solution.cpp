9cf0328e45d38d89aad654e09159d4c5b853181f
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-23 15:58:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (42.11%)

--- 1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
@@ -5,16 +5,14 @@ public:
         //odd ->  -1  means last bit 1->0
         //8 & 1 ->> (1000 & 1) ->> (1000 & 0001) ->> 0000
         //3 & 1 ->> 11 & 1 ->> 01
-        //so num&1 gives 0(even) , 1(odd)
+        //so num&1 gives ->  0(even) , 1(odd)
+        //steps to do no.->  1(/2)   , 2(-1,/2)
+        if(num==0)return 0;
         int ans=0;
         while(num){
-            if(num&1){
-                num-=1;
-            }else{
-                num>>=1;
-            }
-            ans++;
+            ans+= (num&1)?2:1;
+            num>>=1;
         }
-        return ans;
+        return ans-1;
     }
 };
