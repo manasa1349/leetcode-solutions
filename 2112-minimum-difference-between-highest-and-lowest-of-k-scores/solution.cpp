7286445b451c7c17a6fcd3045842e22c5e71e64e
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-25 15:45:55 +0000

    Sync LeetCode submission Runtime - 4 ms (26.35%), Memory - 17.2 MB (90.18%)

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/solution.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/solution.cpp
@@ -5,12 +5,8 @@ public:
         if(n==1 || k==1) return 0;
         sort(nums.begin(),nums.end());
         int ans=INT_MAX;
-        for(int i=0;i<=n-k;i++){
-            int val=nums[i+k-1]-nums[i];
-            // if(i>=k-1){
-                ans=min(ans,val);
-            // }
-            cout<<val<<" ";
+        for(int i=0;i+k-1<n;i++){
+            ans=min(ans,nums[i+k-1]-nums[i]);
         }
         return ans;
     }
