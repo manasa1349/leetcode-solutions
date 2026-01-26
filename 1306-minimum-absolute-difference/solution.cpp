86efe2a49bc582074e1f9bb2878d73dea68f2028
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-01-26 05:17:13 +0000

    Sync LeetCode submission Runtime - 15 ms (76.40%), Memory - 36.7 MB (31.70%)

--- 1306-minimum-absolute-difference/solution.cpp
+++ 1306-minimum-absolute-difference/solution.cpp
@@ -3,12 +3,13 @@ public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin(),arr.end());
         int mn=INT_MAX;
-        for(int i=0;i<arr.size()-1;i++){
-            mn=min(mn,arr[i+1]-arr[i]);
-        }
         vector<vector<int>>res;
         for(int i=0;i<arr.size()-1;i++){
-            if(arr[i+1]-arr[i] == mn){
+            if(arr[i+1]-arr[i]<mn){
+                mn=arr[i+1]-arr[i];
+                res.clear();
+                res.push_back({arr[i],arr[i+1]});
+            }else if(arr[i+1]-arr[i] == mn){
                 res.push_back({arr[i],arr[i+1]});
             }
         }
