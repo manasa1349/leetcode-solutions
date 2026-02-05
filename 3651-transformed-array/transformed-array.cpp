15173c12abb14560c4c8b392cb59e4ce0bfb06fc
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-05 06:38:44 +0530

    Time: 4 ms (79.67%) | Memory: 25.7 MB (70.75%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,13 +4,7 @@ public:
         int n=nums.size();
         vector<int>res(n);
         for(int i=0;i<n;i++){
-            int idx=i;
-            if(nums[i]>0){
-                idx=(i+nums[i])%n;
-            }else if(nums[i]<0){
-                idx=(i- (abs(nums[i])%n) +n)%n;
-            }
-            res[i]=nums[idx];
+            res[i]=nums[((i+nums[i])%n+n)%n];
         }
         return res;
     }
