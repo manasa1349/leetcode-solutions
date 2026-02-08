72b924dfdaa3fa81d9276f43c6e4db89648f911f
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-08 18:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (82.16%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -15,13 +15,18 @@ public:
         if(!root){
             return 0;
         }
-        return 1+max(height(root->left),height(root->right));
+        int l=height(root->left);
+        int r=height(root->right);
+        if(l==-1 || r==-1){
+            return -1;
+        }
+        if(abs(l-r)>1){
+            return -1;
+        }
+        return 1+max(l,r);
     }
     bool isBalanced(TreeNode* root) {
         if(!root) return true;
-        if(abs(height(root->left)-height(root->right))>1){
-            return false;
-        }
-        return isBalanced(root->left)&&isBalanced(root->right);
+        return height(root)!=-1;
     }
 };
