aa77c1485b32e35befd268b40ba5326a33c953e8
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-16 15:16:32 +0530

    Time: 11 ms (11.29%) | Memory: 26.8 MB (18.01%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool func(int l,int r,string s){
-        if(l==r) return true;
         while(l<=r){
             if(s[l]!=s[r]){
                 return false;
@@ -14,22 +13,13 @@ public:
     bool validPalindrome(string s) {
         int n=s.size();
         int l=0,r=n-1;
-        bool chk=false;
         while(l<=r){
-            if(s[l]!=s[r]){
-                if(!chk){
-                    if(l==r || func(l+1,r,s) || func(l,r-1,s)){
-                        return true;
-                    }else{
-                        return false;
-                    }
-                    chk=true;
-                }else{
-                    return false;
-                }
+            if(s[l]==s[r]){
+                l++;
+                r--;
+            }else{
+               return (func(l+1,r,s) || func(l,r-1,s));        
             }
-            l++;
-            r--;
         }
         return true;
     }
