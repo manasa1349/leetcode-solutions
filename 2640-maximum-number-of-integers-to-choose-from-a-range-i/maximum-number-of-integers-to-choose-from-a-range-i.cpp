8b021787547075f6dc2c63921dca00b8308e1a3b
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-23 15:28:56 +0530

    Time: 278 ms (18.94%) | Memory: 185.5 MB (16.86%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
-    int maxCount(vector<int>& nums, int n, int maxSum) {
-        set<int>s(nums.begin(),nums.end());
-        int cnt=0;
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            if(s.find(i)==s.end()){
-                if(sum+i>maxSum) break;
-                sum+=i;
-                cnt++;
+    int solve(int res,int mid,int n,set<int>& ban,int maxSum){
+        int sum=(mid*(mid+1))/2;
+        res=mid;
+        for(int it:ban){
+           if(it<=mid){
+                res--;
+                sum-=it;
+           }
+           else{
+            break;
+           }
+        }
+        if(sum<=0){
+            return 0;
+        }
+        if(sum<=maxSum){
+            return res;
+        }
+        return 0;
+    }
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        int l=1,h=n,res=0;
+        set<int>ban;
+        for(int it:banned){
+            ban.insert(it);
+        }
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(solve(res,mid,n,ban,maxSum)>0){
+                res=solve(res,mid,n,ban,maxSum);
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
             }
         }
-        return cnt;
+        return res;
     }
 };
