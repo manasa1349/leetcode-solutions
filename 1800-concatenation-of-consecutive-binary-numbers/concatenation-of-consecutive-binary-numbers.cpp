92c494481de65362a1b23b72c8dd4b52ae666b69
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-02-28 15:30:27 +0530

    Time: 29 ms (67.11%) | Memory: 7.9 MB (89.26%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        // string ans="";
-        // for(int i=1;i<=n;i++){
-        //     string binary_str = bitset<32>(i).to_string();
-        //     ans+=binary_str;
-        // }
-        // return stoi(ans, nullptr, 2);
         long long ans = 0;
         int mod = 1e9 + 7;
-        
+        int bit_length=0;
         for (int i = 1; i <= n; i++) {
-            string binary_str = bitset<32>(i).to_string();
-            binary_str.erase(0, min(binary_str.find_first_not_of('0'), binary_str.size() - 1));
-            
-            for (char c : binary_str) {
-                ans = (ans * 2 + (c - '0')) % mod;
+            if ((i & (i - 1)) == 0) {
+                bit_length++;
             }
+            ans = (ans << bit_length | (i)) % mod;
         }
         return (int)ans;
     }
