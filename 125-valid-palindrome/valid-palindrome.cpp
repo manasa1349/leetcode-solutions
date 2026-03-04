5463735417cba18abb6ec4d6a09d748ce512ff8b
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-03-04 13:45:40 +0530

    Time: 2 ms (37.64%) | Memory: 10.3 MB (24.03%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string li="";
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(isalnum(s[i])){
+                li+=(tolower(s[i]));
+            }
+        }
+        int m=li.size();
+        for(int i=0;i<m/2;i++){
+            if(li[i]!=li[m-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
