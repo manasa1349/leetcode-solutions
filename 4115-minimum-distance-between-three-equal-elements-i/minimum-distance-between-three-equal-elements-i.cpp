8b0aa7ee50f18377e51e1ddd04e0266cba7c7d3e
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-04-10 19:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (26.26%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        unordered_map<int,vector<int>>mp;
-        int ans=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]].push_back(i);
-        }
-        bool chk=false;
-        for(auto i:mp){
-            int n=i.second.size();
-            if(n>=3){
-                chk=true;
+        int n = nums.size();
+        // Since nums[i] <= n, we can use a 2D array to store up to 3 most recent indices
+        // indices[val][0] = count, indices[val][1-3] = the last 3 positions
+        int last[101][4] = {0}; 
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            int val = nums[i];
+            last[val][0]++; // increment count
+            
+            // Shift indices to keep the last three
+            last[val][1] = last[val][2];
+            last[val][2] = last[val][3];
+            last[val][3] = i;
+
+            // If we have seen this number at least 3 times
+            if (last[val][0] >= 3) {
+                // Distance = 2 * (current_index - index_from_2_occurrences_ago)
+                ans = min(ans, 2 * (last[val][3] - last[val][1]));
             }
-            for(int j=0;j<=n-3;j++){
-                int diff=(  (i.second[j+2]-i.second[j+1]) + (i.second[j+1]-i.second[j]) + (i.second[j+2]-i.second[j]) );
-                ans=min(ans,diff);
-            }
         }
-        if(!chk) return -1;
-        return ans;
+
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
