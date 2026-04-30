1981bf62cbb252e28cddb90eac44766b16a231d2
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-04-30 11:13:57 +0530

    Time: 714 ms (86.06%) | Memory: 499.3 MB (78.75%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int func(int i,int j,int n,int m,int cost,int k,vector<vector<int>>&arr,vector<vector<vector<int>>>&dp){
-        if(j>m-1 || i>n-1){
-            return -1e9;
-        }
-        if(arr[i][j]==1){
-            cost+=1;
-        }else if(arr[i][j]==2){
-            cost+=1;
-        }
-        if(cost>k) return -1e9;
-        if(i==n-1 && j==m-1){
-            return arr[i][j];
-        }
-        int right=func(i,j+1,n,m,cost,k,arr,dp);
-        int down=func(i+1,j,n,m,cost,k,arr,dp);
-        int best=max(right,down);
-        if(best == -1e9) return dp[i][j][cost]=-1e9;
-        return dp[i][j][cost]=arr[i][j]+best;
-    }
+    // int func(int i,int j,int n,int m,int cost,int k,vector<vector<int>>&arr,vector<vector<vector<int>>>&dp){
+    //     if(j>m-1 || i>n-1){
+    //         return -1e9;
+    //     }
+    //     if(arr[i][j]==1){
+    //         cost+=1;
+    //     }else if(arr[i][j]==2){
+    //         cost+=1;
+    //     }
+    //     if(cost>k) return -1e9;
+    //     if(i==n-1 && j==m-1){
+    //         return arr[i][j];
+    //     }
+    //     int right=func(i,j+1,n,m,cost,k,arr,dp);
+    //     int down=func(i+1,j,n,m,cost,k,arr,dp);
+    //     int best=max(right,down);
+    //     if(best == -1e9) return dp[i][j][cost]=-1e9;
+    //     return dp[i][j][cost]=arr[i][j]+best;
+    // }
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(k+1,-1)));
