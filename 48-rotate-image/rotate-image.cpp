85d4cf9934e88d6a1d6946e992fd150605573040
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-05-04 17:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.70%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-
-        for(int layer = 0; layer < n / 2; layer++) {
-            int first = layer;
-            int last = n - layer - 1;
-
-            for(int i = first; i < last; i++) {
-                int offset = i - first;
-
-                int top = matrix[first][i];
-
-                // left → top
-                matrix[first][i] = matrix[last - offset][first];
-
-                // bottom → left
-                matrix[last - offset][first] = matrix[last][last - offset];
-
-                // right → bottom
-                matrix[last][last - offset] = matrix[i][last];
-
-                // top → right
-                matrix[i][last] = top;
+    void rotate(vector<vector<int>>& a) {
+        int n=a.size();
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(a[i][j],a[j][i]);
             }
+        }
+
+        //reverse ele in each row
+        for(int i=0;i<n;i++){
+            reverse(a[i].begin(),a[i].end());
         }
     }
 };
