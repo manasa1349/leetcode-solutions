2edd69fe011e01f0e7aa46ed2e5d452d54723932
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-05-06 12:00:32 +0530

    Time: 189 ms (86.11%) | Memory: 10.3 MB (92.72%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool func(vector<vector<char>>&board, string &word,int idx,int i,int j){
+        if(idx==word.size()){
+            return true;
+        }
+        if(i<0 || i>board.size()-1 || j<0 || j>board[0].size()-1){
+            return false;
+        }
+        if(board[i][j]!=word[idx]){
+            return false;
+        }
+
+        char val=board[i][j];
+        board[i][j]='.';
+        bool ans=
+           func(board,word,idx+1,i-1,j)
+        || func(board,word,idx+1,i+1,j)
+        || func(board,word,idx+1,i,j-1)
+        || func(board,word,idx+1,i,j+1);
+        board[i][j]=val;
+        return ans;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(func(board,word,0,i,j)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
