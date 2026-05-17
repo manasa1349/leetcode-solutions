f41eacd24b96d4c135bdddb24e76291e54daede1
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-05-17 22:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (85.60%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    bool dfs(int i,vector<int>&arr,int n){
-        if(i<0 || i>=n || arr[i]<0){
-            return false;
+    bool bfs(int i,vector<int>&arr,int n){
+        queue<int>q;
+        q.push(i); //st idx
+        while(!q.empty()){
+            int idx=q.front();
+            q.pop();
+            if(arr[idx]==0) return true;//condition check
+            if(arr[idx]<0) continue; //already visited
+            if(idx+arr[idx]<n){
+                q.push(idx+arr[idx]);
+            }
+            if(idx-arr[idx]>=0){
+                q.push(idx-arr[idx]);
+            }
+            arr[idx]=-arr[idx]; //making negative
         }
-        if(arr[i]==0){
-            return true;
-        }
-        arr[i]=-arr[i];
-        return (dfs(i+arr[i],arr,n)) || (dfs(i-arr[i],arr,n));
+        return false;
     }
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
-        return dfs(start,arr,n);
+        return bfs(start,arr,n);
     }
 };
