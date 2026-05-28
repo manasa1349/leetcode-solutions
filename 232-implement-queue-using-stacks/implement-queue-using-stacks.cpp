22855c95c1fd5ce82efde89b788683dc4cf0edd7
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-05-28 21:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (67.86%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -11,34 +11,30 @@ public:
     }
     
     int pop() {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
         }
         int val=st2.top();
         st2.pop();
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
         return val;
     }
     
     int peek() {
-         while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
         }
         int val=st2.top();
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
         return val;
     }
     
     bool empty() {
-        if(st1.empty()){
+        if(st1.empty() && st2.empty()){
             return true;
         }
         return false;
