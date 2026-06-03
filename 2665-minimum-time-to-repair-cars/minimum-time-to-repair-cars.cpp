c25db09c3f7f18acdb6855f3c35fec0134f85a43
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-06-03 14:23:09 +0530

    Time: 24 ms (74.81%) | Memory: 59.3 MB (49.23%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool check(long long t,vector<int>&ranks,int cars){
+        long long car=0;
+        for(long long i=0;i<ranks.size();i++){
+            car+=(long long)sqrt(t/ranks[i]);
+            if(car>=cars) return true;
+        }
+        return false;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        //binary search on time t= ranks[i]*n*n
+                            // t=ranks* n^2
+                            // t/rank=n^2
+                            // (car done in time t) n=sqrt(t/rank)
+        long long l=1; //min time
+        long long h=(*min_element(ranks.begin(),ranks.end()))*1LL*cars*cars;//max possible min time
+        long long ans=0;
+        while(l<=h){
+            long long mid=l+(h-l)/2;
+            if(check(mid,ranks,cars)){
+                ans=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
