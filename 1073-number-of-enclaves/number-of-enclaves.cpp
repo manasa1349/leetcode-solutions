54cc380e8854cea9bb0f0b3f3aa2a4c22f42db37
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-06-03 10:41:07 +0530

    Time: 9 ms (49.34%) | Memory: 28.8 MB (97.17%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -3,33 +3,27 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>arr=grid;
         queue<pair<int,int>>q;
         //1st and last col
         for(int i=0;i<n;i++){
             if(grid[i][0]==1){
                 q.push({i,0});
-                vis[i][0]=1;
-                arr[i][0]=0;
+                grid[i][0]=0;
             }
             if(grid[i][m-1]==1){
                 q.push({i,m-1});
-                vis[i][m-1]=1;
-                arr[i][m-1]=0;
+                grid[i][m-1]=0;
             }
         }
         //1st and last col
         for(int i=0;i<m;i++){
             if(grid[0][i]==1){
                 q.push({0,i});
-                vis[0][i]=1;
-                arr[0][i]=0;
+                grid[0][i]=0;
             }
             if(grid[n-1][i]==1){
                 q.push({n-1,i});
-                vis[n-1][i]=1;
-                arr[n-1][i]=0;
+                grid[n-1][i]=0;
             }
         }
         int delta_i[]={0,0,-1,1};
@@ -41,17 +35,16 @@ public:
             for(int k=0;k<4;k++){
                 int ni=i+delta_i[k];
                 int nj=j+delta_j[k];
-                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1 && !vis[ni][nj]){
+                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
                     q.push({ni,nj});
-                    vis[ni][nj]=1;
-                    arr[ni][nj]=0;
+                    grid[ni][nj]=0;
                 }
             }
         }
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(arr[i][j]==1){
+                if(grid[i][j]==1){
                     ans++;
                 }
             }
