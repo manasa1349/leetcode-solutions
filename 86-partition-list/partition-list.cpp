e79d532d658188087dae6fbe05059c6eda407b87
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-06-08 22:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (41.98%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -13,36 +13,22 @@ public:
     ListNode* partition(ListNode* head, int x) {
 
         if(!head) return nullptr;
+        ListNode *lft=new ListNode(-1);
+        ListNode *rgt=new ListNode(-1);
+        ListNode *lftdummy=lft, *rgtdummy=rgt,*temp=head;
 
-        vector<int>lft,rgt;
-        ListNode *temp=head;
         while(temp){
             if(temp->val<x){
-                lft.push_back(temp->val);
-            }else if(temp->val>=x){
-                rgt.push_back(temp->val);
+                lft->next=temp;
+                lft=lft->next;
+            }else{
+                rgt->next=temp;
+                rgt=rgt->next;
             }
             temp=temp->next;
         }
-
-        int idx=0;
-        if(lft.size()!=0){
-            temp=new ListNode(lft[0]);
-        }else if(lft.empty() && !rgt.empty()){
-            temp=new ListNode(rgt[0]);
-            idx=1;
-        }
-        ListNode *ans=temp;
-        for(int i=1;i<lft.size();i++){
-            ListNode* node=new ListNode(lft[i]);
-            temp->next=node;
-            temp=temp->next;
-        }
-        for(int i=idx;i<rgt.size();i++){
-            ListNode* node=new ListNode(rgt[i]);
-            temp->next=node;
-            temp=temp->next;
-        }
-        return ans;
+        rgt->next=nullptr;
+        lft->next=rgtdummy->next;
+        return lftdummy->next;
     }
 };
