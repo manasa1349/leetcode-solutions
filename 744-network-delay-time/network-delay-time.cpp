22667c843997eee5d478ce2393485a91312775ca
manasa1349 <147472718+manasa1349@users.noreply.github.com>
2026-09-07 20:46:14 +0530

    Time: 83 ms (56.86%) | Memory: 44.5 MB (54.81%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // Dijkstra Algo
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(int i=0;i<times.size();i++){
+            int u=times[i][0],v=times[i][1],w=times[i][2];
+            adj[u].push_back({v,w});
+        }
+
+                    //time/dist, node
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
+        vector<int>dist(n+1,INT_MAX);
+
+        pq.push({0,k});
+        dist[k]=0;
+
+        while(!pq.empty()){
+            int tm=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+
+            if(tm > dist[node]) continue;
+
+            for(auto it:adj[node]){
+                int node2=it.first;
+                int time=it.second;
+                if(dist[node]+time < dist[node2]){
+                    dist[node2]=dist[node]+time;
+                    pq.push({dist[node2],node2});
+                }
+            }
+        }
+        int ans=INT_MIN;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==INT_MAX) return -1;
+            if(i==k) continue;
+            ans=max(dist[i],ans);
+        }
+        return ans;
+    }
+};
